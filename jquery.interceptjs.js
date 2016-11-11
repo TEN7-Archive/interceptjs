30cc6790f32b01e95056f83ae7a6260c35afb62f
Ivan Stegic <ivan.stegic@gmail.com>
2016-11-11 17:57:20 -0600

    Removed loading of js-cookie in the plugin itself. Now requires that cookie and js-modal be included before this in the html. Moved showIntercept() function down to a more suitable place in the code.

--- jquery.interceptjs.js
+++ jquery.interceptjs.js
@@ -1,5 +1,15 @@
 (function($) {
     $.fn.interceptjs = function(options) {
+        // https://github.com/js-cookie/js-cookie
+        if (!window.Cookies) {
+            console.log('js-cookie is not installed. See http://interceptjs.io/ for help.');
+            return;
+        }
+        // https://github.com/kylefox/jquery-modal
+        if (!$.modal) {
+            console.log('jquery-modal is not installed. See http://interceptjs.io/ for help.');
+            return;
+        }
         // Establish our default settings
         var settings = $.extend({
             pagenum: 0,
@@ -23,7 +33,72 @@
         // Banner alert
         var html_banner = '<div id="ijs_banner" class="ijs_intercept"><div id="ijs_banner_close" class="ijs_close">x</div>' + settings.text + '</div>';
         // Modal alert
-        var html_modal = '<div id="ijs_modal" class="ijs_intercept"><div id="ijs_modal_close" class="ijs_close">x</div>' + settings.text + '</div>';
+        var html_modal = '<div id="ijs_modal" class="ijs_intercept">' + settings.text + '</div>';
+        // Return this
+        return this.each(function() {
+            // Is this thing even on?
+            if (settings.power_switch == 0) {
+                // Do nothing
+                return;
+            }
+            // Some variables
+            var ts = Math.floor(Date.now());
+            var rndnum = Math.floor(Math.random() * 100); // between 0 and 100
+            // Load user data from cookie, setup defaults if none
+            ijs = Cookies.getJSON('ijs') || {};
+            if ($.isEmptyObject(ijs)) {
+                // User data
+                ijs = {
+                    user_dismiss: 0,
+                    pageview: 0,
+                    first_visit: ts,
+                    last_visit: ts
+                };
+                Cookies.set('ijs', ijs);
+            }
+            // Should we start a new session?
+            var session_duration = settings.session_duration * 3600000;
+            var elapsed_time = ijs.last_visit + session_duration;
+            if ((ts > elapsed_time) || ts == ijs.first_visit) {
+                // New session
+                ijs.pageview = 1;
+            } else {
+                // Continue existing session
+                ijs.pageview++;
+            }
+            // Update last visit
+            ijs.last_visit = ts;
+            // Save cookie
+            Cookies.set('ijs', ijs);
+            // User dismiss on?
+            if (ijs.user_dismiss == 1) {
+                if (settings.user_override == 1) {
+                    ijs.user_dismiss = 0;
+                    Cookies.set('ijs', ijs);
+                } else {
+                    // Do nothing
+                    return;
+                }
+            }
+            // Check the page we're on
+            if (settings.pagenum == 0 || settings.pagenum == ijs.pageview) {
+                // What percentage of the time to show?
+                if (rndnum < settings.percent) {
+                    // Show the intercept
+                    showIntercept();
+                } else {
+                    // Do nothing
+                    return;
+                }
+            } else {
+                // Do nothing
+                return;
+            }
+            // Complete
+            if ($.isFunction(settings.complete)) {
+                settings.complete.call(this);
+            }
+        });
         // Show intercept logic
         function showIntercept() {
             // Wait for show_delay seconds before actually showing anything
@@ -38,7 +113,7 @@
                         break;
                         // Modal
                     case 'modal':
-                        $('body').prepend(html_modal).show();
+
                         break;
                         // Banner
                     case 'banner':
@@ -65,72 +140,5 @@
                 return;
             }, settings.show_delay);
         }
-        // Return this
-        return this.each(function() {
-            // Is this thing even on?
-            if (settings.power_switch == 0) {
-                // Do nothing
-                return;
-            }
-            // Load cookie library
-            $.getScript('https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.3/js.cookie.min.js', function(data, textStatus, jqxhr) {
-                // Some variables
-                var ts = Math.floor(Date.now());
-                var rndnum = Math.floor(Math.random() * 100); // between 0 and 100
-                // Load user data from cookie, setup defaults if none
-                ijs = Cookies.getJSON('ijs') || {};
-                if ($.isEmptyObject(ijs)) {
-                    // User data
-                    ijs = {
-                        user_dismiss: 0,
-                        pageview: 0,
-                        first_visit: ts,
-                        last_visit: ts
-                    };
-                    Cookies.set('ijs', ijs);
-                }
-                // Should we start a new session?
-                var session_duration = settings.session_duration * 3600000;
-                var elapsed_time = ijs.last_visit + session_duration;
-                if ((ts > elapsed_time) || ts == ijs.first_visit) {
-                    // New session
-                    ijs.pageview = 1;
-                } else {
-                    // Continue existing session
-                    ijs.pageview++;
-                }
-                // Update last visit
-                ijs.last_visit = ts;
-                // Save cookie
-                Cookies.set('ijs', ijs);
-                // User dismiss on?
-                if (ijs.user_dismiss == 1) {
-                    if (settings.user_override == 1) {
-                        ijs.user_dismiss = 0;
-                        Cookies.set('ijs', ijs);
-                    } else {
-                        // Do nothing
-                        return;
-                    }
-                }
-                // Check the page we're on
-                if (settings.pagenum == 0 || settings.pagenum == ijs.pageview) {
-                    // What percentage of the time to show?
-                    if (rndnum < settings.percent) {
-                        // Show the intercept
-                        showIntercept();
-                    } else {
-                        // Do nothing
-                        return;
-                    }
-                } else {
-                    // Do nothing
-                    return;
-                }
-            });
-            if ($.isFunction(settings.complete)) {
-                settings.complete.call(this);
-            }
-        });
     };
 }(jQuery));
